3379e9992522f75fd10aef84d4b6a79d6d146877
Mike Smart <mike.smart@jadu.co.uk>
2018-02-08 21:00:35 +0000

    move sprites into state, add comments

--- src/objects/Carousel.ts
+++ src/objects/Carousel.ts
@@ -1,8 +1,4 @@
 import 'phaser-ce';
-import '../../assets/sheets/catalogue/catalogue.json';
-import '../../assets/sheets/catalogue/catalogue.png';
-import '../../assets/sheets/products/products@4x.json';
-import '../../assets/sheets/products/products@4x.png';
 import CatalogueService from '../services/CatalogueService';
 import { CatalogueInterface, UserCatalogueInterface, UserProductInterface } from '../interfaces/catalogue';
 import Product from './Product';
@@ -30,25 +26,18 @@ export default class Carousel extends Phaser.Sprite {
     ) {
         super(game, x, y);
 
-        // catalogue
+        // Create new catalogue service for managing items
         this.catalogue = new CatalogueService(items);
-    }
-
-    public preload (
-        game: Phaser.Game
-    ) {
-        game.load.atlas('catalogue', 'catalogue.png', 'catalogue.json');
-        game.load.atlas('products', 'products@4x.png', 'products@4x.json');
     }
 
     public create (
         game: Phaser.Game
     ) {
-        // arrows
+        // Create arrow sprites
         this.leftArrow = new Phaser.Sprite(game, 0, 0, 'catalogue', sprites.ARROW);
         this.rightArrow = new Phaser.Sprite(game, 0, 0, 'catalogue', sprites.ARROW);
 
-        // background
+        // Create background which will give the Carousel it's bounds
         this.background = new Phaser.Graphics(game, 0, 0);
 
         // create left arrow from right arrow sprite
@@ -58,37 +47,41 @@ export default class Carousel extends Phaser.Sprite {
         this.leftArrow.anchor.setTo(0.5);
         this.rightArrow.anchor.setTo(0.5);
 
-        // product
+        // Create new Product from the active item
         this.item = new Product(game, this.catalogue.getActiveProduct());
 
-        // wrapper
+        // Add objects
         this.addChild(this.background);
         this.addChild(this.item);
         this.addChild(this.leftArrow);
         this.addChild(this.rightArrow);
 
-        // background
-        this.background.beginFill(0x0000ff);
+        // Set background dimensions and fill
+        this.background.beginFill(0x000000);
+        // Make background fully transparent
         this.background.alpha = 0;
+        // Draw background rect
         this.background.drawRect(0, 0, game.width, game.height / 2);
 
+        // FIXME: This is the global res value hardcoded
         this.topPadding = 32;
 
-        // carousel positioning
+        // Position carousel
         this.x = game.world.centerX - (this.background.width / 2);
         this.y = 0;
 
-        // arrow positioning
+        // Arrow padding on the X axis from the edge of the bounding Background object
         const arrowPadding = 60;
 
+        // Position arrows
         this.leftArrow.y = (this.background.height / 2) - (this.leftArrow.height / 2) + this.topPadding;
         this.leftArrow.x = (this.leftArrow.width / 2) + arrowPadding;
         this.rightArrow.y = (this.background.height / 2) - (this.rightArrow.height / 2) + this.topPadding;
         this.rightArrow.x = (this.background.width - (this.rightArrow.width / 2)) - arrowPadding;
 
-        // product positioning
+        // Position product
         this.item.x = (this.background.width / 2) - (this.item.getDimensions().width / 2);
-        this.item.y = 16 + this.topPadding;
+        this.item.y = this.topPadding * 1.5;
     }
 
     public handleKeyDown (
@@ -96,24 +89,40 @@ export default class Carousel extends Phaser.Sprite {
     ) {
         switch (game.input.keyboard.event.keyCode) {
             case Phaser.Keyboard.LEFT:
+                // Scale arrow up
                 this.leftArrow.scale.setTo(this.arrowScale);
+                // Get previous item
                 this.catalogue.prev();
                 break;
             case Phaser.Keyboard.RIGHT:
+                // Scale arrow up
                 this.rightArrow.scale.setTo(this.arrowScale);
+                // Get next item
                 this.catalogue.next();
                 break;
             }
 
+        // Update active product
         this.item.updateProduct(this.catalogue.getActiveProduct());
     }
 
     public handleKeyUp () {
+        // Normalize arrow scale
         this.leftArrow.scale.setTo(1);
         this.rightArrow.scale.setTo(1);
     }
 
     public getActiveItem (): UserProductInterface {
+        // Return active item
         return this.catalogue.getActiveProduct();
+    }
+
+    public setArrowPositionY (
+        unit: number
+    ) {
+        // FIXME: add DEFAULT_RES to game maybe?
+        // need to stop hardcoding 32 everywhere!
+        this.leftArrow.y += (unit * 32);
+        this.rightArrow.y += (unit * 32);
     }
 }
