211d5857e925bfbe538c63a27ef4ee702c4b52af
Mike Smart <mike.smart@jadu.co.uk>
2018-02-09 09:43:55 +0000

    tidy up player item service

--- src/services/PlayerItemService.ts
+++ src/services/PlayerItemService.ts
@@ -6,17 +6,25 @@ import {
 } from '../interfaces/catalogue';
 
 export default class PlayerItemService {
+    /**
+     * Master Collection of all catalogues
+     */
     private master: UserCatalogueInterface[];
 
+    /**
+     * Player Item Service
+     * @param master
+     */
     constructor (
         master: CatalogueInterface[]
     ) {
         // Create a recursive clone of the master catalogue collection
-        this.master = master.map((c: CatalogueInterface) => {
-            return Object.assign({}, c, {
-                products: c.products.map((p: ProductInterface) => {
+        this.master = master.map((catalogue: CatalogueInterface) => {
+            return Object.assign({}, catalogue, {
+                products: catalogue.products.map((product: ProductInterface) => {
+                    // Add locked & owned default values to products
                     return {
-                        product: Object.assign({}, p),
+                        product: Object.assign({}, product),
                         locked: true,
                         owned: false
                     };
@@ -24,35 +32,89 @@ export default class PlayerItemService {
             });
         });
 
-        // default unlocks
-        // "The Everyday"
-        this.master.find((c: UserCatalogueInterface) => c.id === 'INTRO')
-            .products.find((p: UserProductInterface) => p.product.id === 0)
+        // default unlocks, FIXME: This could be done in a more efficient way,
+        // possibly conifg driven, this will get out of hand at scale
+
+        // Unlock the "The Everyday" by default
+        this.master.find((catalogue: UserCatalogueInterface) => catalogue.id === 'INTRO')
+            .products.find((item: UserProductInterface) => item.product.id === 0)
             .locked = false;
     }
 
-    public getItemsById (
+    /**
+     * Get a catalogue by ID
+     * @param key
+     */
+    public getCatalogueById (
         key: string
     ): UserCatalogueInterface {
-        return this.master.find((c: UserCatalogueInterface) => c.id === key);
+        // Get a catalogue by ID
+        return this.master.find((catalogue: UserCatalogueInterface) => catalogue.id === key);
     }
 
-    public purchase (
+    /**
+     * Buy an item
+     * @param catalogueId
+     * @param item
+     */
+    public buy (
         catalogueId: string,
-        product: UserProductInterface
+        item: UserProductInterface
     ) {
-        // Create new immutable set of products
-        const newProducts = this.getItemsById(catalogueId).products.map((p: UserProductInterface) => {
-            if (p.product.id === product.product.id) {
-                return Object.assign({}, p, {
-                    owned: true
-                });
-            } else {
-                return p;
-            }
-        });
+        // FIXME: fix duplication of buy/sell methods, these should invoke a private update method
+        // that receives the new state diff and merges with the catalogue state
 
-        // update products
-        this.getItemsById(catalogueId).products = newProducts;
+        // Create new set of products in catalogue with state changes
+        // update catalogue with new product state
+        this.getCatalogueById(catalogueId).products = this.getCatalogueById(catalogueId).products
+            .map((catalgueItem: UserProductInterface) => {
+                if (catalgueItem.product.id === item.product.id) {
+                    return Object.assign({}, catalgueItem, {
+                        owned: true
+                    });
+                }
+
+                return catalgueItem;
+            });
+    }
+
+    /**
+     * Sell an item
+     * @param catalogueId
+     * @param item
+     */
+    public sell (
+        catalogueId: string,
+        item: UserProductInterface
+    ) {
+        // FIXME: fix duplication of buy/sell methods, these should invoke a private update method
+        // that receives the new state diff and merges with the catalogue state
+
+        // Create new set of products in catalogue with state changes
+        // update catalogue with new product state
+        this.getCatalogueById(catalogueId).products = this.getCatalogueById(catalogueId).products
+            .map((catalgueItem: UserProductInterface) => {
+                if (catalgueItem.product.id === item.product.id) {
+                    return Object.assign({}, catalgueItem, {
+                        owned: false
+                    });
+                }
+
+                return catalgueItem;
+            });
+    }
+
+    /**
+     * Check if an item is owned by the current player
+     * @param catalogueId
+     * @param product
+     */
+    public owned (
+        catalogueId: string,
+        item: UserProductInterface
+    ): boolean {
+        return this.getCatalogueById(catalogueId).products.find((catalgueItem: UserProductInterface) => {
+            return catalgueItem.product.id === item.product.id;
+        }).owned;
     }
 }
